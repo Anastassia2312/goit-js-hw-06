33636af722fb6565a99e16bbe2fe075f31e81941
Anastassia2312 <23122000na@gmail.com>
2023-11-17 15:41:36 +0200

    tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-/*"use strict";
+"use strict";
 
 const customer = {
   username: "Mango",
@@ -29,4 +29,4 @@ customer.setDiscount(0.15);
 console.log(customer.getDiscount()); // 0.15
 customer.addOrder(5000, "Steak");
 console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]*/
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
